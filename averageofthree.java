901372c30ca37c9bbfd0e4f0b927f7d66e9b97f6
Aryan Gupta <96718347+aryan29gupta@users.noreply.github.com>
2024-11-18 01:17:10 +0530

    Add files via upload

---
+++ averageofthree.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class averageofthree {
+    public static int avgThree(int a , int b , int c ){
+        int avr= (a+b+c)/3;
+        return avr;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int avr= avgThree(a,b,c);
+        System.out.println("AVERAGE OF THREE NUMBERS IS : "+ avr);
+
+    }
+
+
+}
